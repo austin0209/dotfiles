633c7c50ec6b63c3d0ba63b62866b774272bfc43
Austin Aurelio <austin@delveam.com>
2022-05-12 23:19:25 -0500

    fix up nvim setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -60,10 +60,19 @@ vim.o.termguicolors = true
 vim.cmd([[colorscheme gruvbox]])
 
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>", "<nop>")
 vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>p", "<cmd>Telescope lsp_document_symbols<CR>")
 vim.keymap.set("n", "<C-_>", "<Plug>(comment_toggle_current_linewise)")
+
+vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+vim.keymap.set("n", "<C-q>", "<cmd>lua vim.lsp.buf.hover()<CR>")
+vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>")
+vim.keymap.set("n", "<A-CR>", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 
 vim.keymap.set("x", "<C-_>", "<Plug>(comment_toggle_linewise_visual)")
 
