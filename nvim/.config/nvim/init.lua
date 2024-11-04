fa27e6a11d95fabe7e0dccf99891bccaa3e3599f
Austin Aurelio <austin@delveam.com>
2024-11-04 00:39:06 -0600

    WIP: use lazy for nvim package management
    Will eventually configure neovim for clojure specifically.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,27 @@
+-- This is the single file setup taken from lazy
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
@@ -15,51 +39,8 @@ vim.o.mouse = "a"
 vim.o.autoread = true
 vim.o.list = true
 vim.o.listchars = "tab:│ ,trail:¤"
-vim.o.colorcolumn = 80
+vim.o.colorcolumn = "80"
 vim.o.clipboard = "unnamedplus"
-
--- Install packer
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-end
-
-local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
-vim.api.nvim_create_autocmd(
-	"BufWritePost",
-	{ command = "source <afile> | PackerCompile", group = packer_group, pattern = "init.lua" }
-)
-
-require("packer").startup(function(use)
-	use("wbthomason/packer.nvim") -- Package manager
-	use("numToStr/Comment.nvim") -- "gc" to comment visual regions/lines
-	-- UI to select things (files, grep results, open buffers...)
-	use({ "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } })
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	-- use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-	-- Add indentation guides even on blank lines
-	use("lukas-reineke/indent-blankline.nvim")
-	-- Add git related info in the signs columns and popups
-	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
-	-- Highlight, edit, and navigate code using a fast incremental parsing library
-	use("nvim-treesitter/nvim-treesitter")
-	-- Additional textobjects for treesitter
-	use("nvim-treesitter/nvim-treesitter-textobjects")
-	use("neovim/nvim-lspconfig") -- Collection of configurations for built-in LSP client
-	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("saadparwaiz1/cmp_luasnip")
-	use("L3MON4D3/LuaSnip") -- Snippets plugin
-	use("ellisonleao/gruvbox.nvim")
-	use("gpanders/editorconfig.nvim")
-end)
-
-vim.o.termguicolors = true
-vim.cmd([[colorscheme gruvbox]])
-
-vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>", "<nop>")
 vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>")
@@ -79,53 +60,15 @@ vim.keymap.set("x", "<C-_>", "<Plug>(comment_toggle_linewise_visual)")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
-require("indent_blankline").setup({
-	show_trailing_blankline_indent = false,
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
 })
 
-require("Comment").setup()
-
-require("gitsigns").setup()
-
-require("nvim-treesitter.configs").setup({
-	ensure_installed = "all",
-	highlight = {
-		enable = true,
-	},
-})
-
--- Setup nvim-cmp.
-local cmp = require("cmp")
-
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-		end,
-	},
-	window = {
-		completion = cmp.config.window.bordered(),
-		documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-b>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-		["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select })),
-		["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select })),
-	}),
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" }, -- For luasnip users.
-	}, {
-		{ name = "buffer" },
-	}),
-})
-
--- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require("lspconfig")["ccls"].setup({
-	capabilities = capabilities,
-})
