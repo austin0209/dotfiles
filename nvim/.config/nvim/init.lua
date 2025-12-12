0d710a994e295e3d23c31d73367e5e8d11eaa53d
Austin Aurelio <austinaurelio@protonmail.com>
2025-12-11 18:30:25 -0600

    nvim: Update init.lua from work
    Added c# support and migrated to native LSP

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,28 +1,10 @@
--- This is the single file setup taken from lazy
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" }, { out, "WarningMsg" }, { "\nPress any key to exit..." }, },
-			true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
+-- Basic Neovim settings
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 vim.o.smartindent = true
 vim.o.number = true
 vim.o.wrap = false
@@ -37,120 +19,239 @@ vim.o.mouse = "a"
 vim.o.autoread = true
 vim.o.list = true
 vim.o.listchars = "tab:│ ,trail:¤"
-vim.o.colorcolumn = "120"
+vim.o.colorcolumn = "140"
 vim.o.clipboard = "unnamedplus"
 vim.o.termguicolors = true
 
--- From CoC recommendations
-vim.opt.updatetime = 300
-vim.opt.signcolumn = "yes"
-
-vim.keymap.set("n", "<leader>", "<nop>")
-vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>")
---vim.keymap.set("n", "<leader>p", "<cmd>Telescope lsp_document_symbols<CR>")
---vim.keymap.set("n", "<C-_>", "<Plug>(comment_toggle_current_linewise)")
-
---vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
---vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
---vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
---vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
---vim.keymap.set("n", "<C-q>", "<cmd>lua vim.lsp.buf.hover()<CR>")
---vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<CR>")
---vim.keymap.set("n", "<A-CR>", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-
---vim.keymap.set("x", "<C-_>", "<Plug>(comment_toggle_linewise_visual)")
-
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
-
---- CoC stuff ---
-local keyset = vim.keymap.set
-local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
-keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
-
-keyset("n", "<A-CR>", "<Plug>(coc-codeaction-cursor")
-
--- GoTo code navigation
-keyset("n", "gd", "<Plug>(coc-definition)", { silent = true })
-keyset("n", "gy", "<Plug>(coc-type-definition)", { silent = true })
-keyset("n", "gi", "<Plug>(coc-implementation)", { silent = true })
-keyset("n", "gr", "<Plug>(coc-references)", { silent = true })
-
--- Use to show documentation in preview window
-function _G.show_docs()
-	local cw = vim.fn.expand('<cword>')
-	if vim.fn.index({ 'vim', 'help' }, vim.bo.filetype) >= 0 then
-		vim.api.nvim_command('h ' .. cw)
-	elseif vim.api.nvim_eval('coc#rpc#ready()') then
-		vim.fn.CocActionAsync('doHover')
-	else
-		vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-	end
+-- Setup lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-keyset("n", "<C-q>", '<CMD>lua _G.show_docs()<CR>', { silent = true })
-
--- Symbol renaming
-keyset("n", "<leader>rn", "<Plug>(coc-rename)", { silent = true })
-
--- Formatting selected code
-keyset("x", "<leader>f", "<Plug>(coc-format-selected)", { silent = true })
-keyset("n", "<leader>f", "<Plug>(coc-format-selected)", { silent = true })
-
--- Add `:Format` command to format current buffer
-vim.api.nvim_create_user_command("Format", "call CocAction('format')", {})
-
---- Setup lazy.nvim ---
 require("lazy").setup({
-	spec = {
-		{
-			"nvim-treesitter/nvim-treesitter",
-			build = ":TSUpdate",
-			-- Add languages to be installed here that you want installed for treesitter
-			config = function()
-				local configs = require("nvim-treesitter.configs")
+  spec = {
+    -- Treesitter
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+        local configs = require("nvim-treesitter.configs")
+        configs.setup({
+          ensure_installed = {
+            'markdown', 'bash', 'html', 'css',
+            'clojure', 'java', 'python', 'lua',
+            'c_sharp', 'javascript', 'typescript'
+          },
+          highlight = { enable = true },
+        })
+      end
+    },
 
-				configs.setup({
-					ensure_installed = {
-						'markdown', 'bash', 'html', 'css',
-						'clojure', 'java', 'python', 'lua'
-					},
-					highlight = {
-						enable = true,
-					},
-				})
-			end
-		},
-		"clojure-vim/clojure.vim",
-		"nvim-telescope/telescope.nvim",
-		"ellisonleao/gruvbox.nvim",
-		{ "neoclide/coc.nvim",       branch = "release" },
-		"sainnhe/everforest",
-		"navarasu/onedark.nvim",
-		{ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } },
-		"Olical/conjure",
-		"HiPhish/rainbow-delimiters.nvim",
-		"gpanders/nvim-parinfer",
-		"haishanh/night-owl.vim",
-		"tpope/vim-dispatch",
-		"radenling/vim-dispatch-neovim",
-		"clojure-vim/vim-jack-in",
-		-- The following were recommended plugins that I'm not sure I need.
-		--"tpope/vim-surround",
-		--"guns/vim-sexp",
-		--"tpope/vim-sexp-mappings-for-regular-people",
-	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "habamax" } },
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+    -- Mason with custom registry for Roslyn
+    {
+      "williamboman/mason.nvim",
+      config = function()
+        require("mason").setup({
+          registries = {
+            "github:mason-org/mason-registry",
+            "github:Crashdummyy/mason-registry", -- Custom registry for Roslyn
+          },
+        })
+      end
+    },
+
+    -- Roslyn LSP plugin
+    {
+      "seblyng/roslyn.nvim",
+      ft = "cs",
+      opts = {
+        -- Add any Roslyn-specific configuration here
+      }
+    },
+
+    -- Completion engine
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+      },
+    },
+
+    -- Snippet engine
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+
+    -- FZF/Telescope
+    "nvim-telescope/telescope.nvim",
+    {
+      "junegunn/fzf",
+      run = "./install --all"
+    },
+
+    -- Other plugins
+    "clojure-vim/clojure.vim",
+    "ellisonleao/gruvbox.nvim",
+    "sainnhe/everforest",
+    "navarasu/onedark.nvim",
+    { "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } },
+    "Olical/conjure",
+    "HiPhish/rainbow-delimiters.nvim",
+    "gpanders/nvim-parinfer",
+    "haishanh/night-owl.vim",
+    "tpope/vim-dispatch",
+    "radenling/vim-dispatch-neovim",
+    "clojure-vim/vim-jack-in",
+  },
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
 })
 
-require("nvim-treesitter.install").prefer_git = true
+-- Configure other LSP servers using new API
+vim.lsp.config('*', {
+  root_markers = { '.git', '.gitignore' },
+})
 
+-- TypeScript/JavaScript
+vim.lsp.config('ts_ls', {
+  cmd = { 'typescript-language-server', '--stdio' },
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+})
+
+-- Lua
+vim.lsp.config('lua_ls', {
+  cmd = { 'lua-language-server' },
+  filetypes = { 'lua' },
+  settings = {
+    Lua = {
+      diagnostics = { globals = { 'vim' } }
+    }
+  }
+})
+
+-- Python
+vim.lsp.config('pyright', {
+  cmd = { 'pyright-langserver', '--stdio' },
+  filetypes = { 'python' },
+})
+
+-- HTML
+vim.lsp.config('html', {
+  cmd = { 'vscode-html-language-server', '--stdio' },
+  filetypes = { 'html' },
+})
+
+-- CSS
+vim.lsp.config('cssls', {
+  cmd = { 'vscode-css-language-server', '--stdio' },
+  filetypes = { 'css', 'scss', 'less' },
+})
+
+-- Enable LSP servers (except Roslyn - handled by plugin)
+vim.lsp.enable('ts_ls')
+vim.lsp.enable('lua_ls')
+vim.lsp.enable('pyright')
+vim.lsp.enable('html')
+vim.lsp.enable('cssls')
+
+-- LSP keymaps
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf, silent = true }
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>ac', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+    vim.keymap.set('n', '<C-q>', function()
+      vim.diagnostic.open_float(nil, {
+        border = 'rounded',
+        source = 'always'
+      })
+    end, opts)
+    vim.keymap.set('v', '<leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+-- Completion setup (same as before)
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  })
+})
+
+-- Custom Format command
+vim.api.nvim_create_user_command("Format", function()
+  vim.lsp.buf.format { async = true }
+end, {})
+
+-- Setup other plugins
+require("nvim-treesitter.install").prefer_git = true
 require("gitsigns").setup()
 
+-- Ctrl P
+vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<CR>")
+
+-- Colorscheme
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
-
